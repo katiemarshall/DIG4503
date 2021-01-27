8b19f2013fd0ab1a6153293d41edf4fd415953c0
Kathryn Marshall <katiemarshall06@gmail.com>
2021-01-27 13:45:02 -0500

    lab1

--- Lab1/Person.js
+++ Lab1/Person.js
@@ -1,9 +1,10 @@
-import chalk from 'chalk';
+import chalk, { BackgroundColor } from 'chalk';
 
 class Person {
     constructor(name, favoriteColor) {
       this.name = name; 
       this.favoriteColor = favoriteColor;
+      this.backgroundColors = backgroundColors;
     }
     speak(){
       console.log(chalk.hex(this.favoriteColor)(this.name));
