89c1452930b4e855efe70e180b59917b6b2cd847
Kathryn Marshall <katiemarshall06@gmail.com>
2021-02-05 18:11:13 -0500

    lab3

---
+++ Lab3/lab3app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './homepage';
+
+function App() {
+  const fname - 'Katie';
+  return (
+    <HomePage firstName={fname} />
+  )
+  ;
+}
+
+export default App;
