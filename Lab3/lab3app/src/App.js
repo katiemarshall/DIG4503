9ae94b667e599266fc3c636e41b0c888531ae4f6
Kathryn Marshall <katiemarshall06@gmail.com>
2021-02-22 12:34:05 -0500

    lab5

--- Lab3/lab3app/src/App.js
+++ Lab3/lab3app/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import HomePage from './homepage';
 
 function App() {
-  const fname - 'Katie';
+    const fname - 'Katie';
   return (
     <HomePage firstName={fname} />
-  )
-  ;
-}
+  );
+
+
+  }
 
 export default App;
---
+++ Lab5/lab5app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './components/Posts'
+
+class App extends React.Component{
+  render(){
+    return(
+      <Posts />
+    );
+  }
+}
+
+
+
+export default App;
