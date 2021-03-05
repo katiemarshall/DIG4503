1dbdbdb918c2ea72df5af26bc666cec1ef4331e7
Kathryn Marshall <katiemarshall06@gmail.com>
2021-03-05 16:04:47 -0500

    portfolio1

---
+++ portfolio1/portfolio1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Hello World!</h1>
+      <p>My name is Katie, and this is my simple React App.</p>
+      </div>
+    );
+  }}
+  
+export default App;
