587d2043a93c6ca9a904af4d737c56f89d9d1db6
Kathryn Marshall <katiemarshall06@gmail.com>
2021-04-29 16:58:58 -0400

    portfolio2

---
+++ portfolio2/index.js
@@ -0,0 +1,45 @@
+import Express from 'express';
+
+const App = Express();
+const port = 45030;
+const chalk = require('chalk');
+
+const names = [
+    'Cortney',
+    'Dewayne',
+    'Trenton',
+    'Pamala',
+    'Ettie',
+    'Errol',
+    'Lorrie',
+    'Hang',
+    'Lauryn',
+    'Caterina',
+    'Isa',
+    'Marcela'
+];
+
+console.log(chalk.blue("Greetings, Professor Winter!"));
+
+App.get('/people/:person', (req, res) => {
+    let names = req.params.names;
+    if (names.includes(names)){
+        res.json({ name: "name"});
+    } else{ 
+        res.json({ Name: "Not available"});
+    }
+});
+
+App.get('/search/:name', (req, res) => {
+   const result = names.filter(str => str.includes(req.params.names));
+
+   if (result != 0) {
+       res.json({ search: [names]});
+   } else {
+       res.json({ search: "Not available" });
+   }
+})
+
+App.listen(port, () => {
+
+});
